22d36766650898e6b614261d24c2ba8b83a3ea04
Antonio Vivone <ndonio96@gmail.com>
2019-08-04 17:48:33 +0200

    medium and hackernoon wrapper

---
+++ server/sources/hackernoon_wrapper.js
@@ -0,0 +1,34 @@
+// dependencies
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+// baseURL of the website
+const baseURL = "https://hackernoon.com/tagged/";
+
+module.exports.getPosts = getPosts;
+
+async function getPosts(keyword) {
+
+    try{
+        let response = await axios.get(`${baseURL}${keyword}`);
+        if(response.status == 200){
+            const $ = cheerio.load(response.data);
+
+            let refinedPosts = [];
+
+            // getting all authors and putting in an array
+            let authors = $('.name').toArray();
+            
+            $('.title a').each((i, el) => {
+                let title = el.children[0].data;    //getting title
+                let url = `https://hackernoon.com${el.attribs.href}`;   //getting href of the a element
+                let author = authors[i].children[0].data;   //getting the post's author
+                refinedPosts.push({title: title, author: author,url: url});
+            });
+            console.log(refinedPosts);
+            return refinedPosts;
+        } 
+    }catch (err){
+
+    }
+}
