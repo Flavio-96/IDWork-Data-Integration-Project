f00a532f67e99575b05af2a9aad7af0272e1efb5
Flavio <f.esposito101@studenti.unisa.it>
2019-08-08 01:05:31 +0200

    Initial project setup, also mongo setted as default db

---
+++ IDWork/config/datastores.js
@@ -0,0 +1,23 @@
+/**
+ * Datastores
+ * (sails.config.datastores)
+ *
+ * A set of datastore configurations which tell Sails where to fetch or save
+ * data when you execute built-in model methods like `.find()` and `.create()`.
+ *
+ *  > This file is mainly useful for configuring your development database,
+ *  > as well as any additional one-off databases used by individual models.
+ *  > Ready to go live?  Head towards `config/env/production.js`.
+ *
+ * For more information on configuring datastores, check out:
+ * https://sailsjs.com/config/datastores
+ */
+
+module.exports.datastores = {
+    default: {
+      default: {
+        adapter: 'sails-mongo',
+        url: 'mongodb://root@localhost/IDWork'
+      }
+  },
+};
