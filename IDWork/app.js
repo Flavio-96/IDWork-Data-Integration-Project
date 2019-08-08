32d3867a3d1f68a1aa0699089df55de32fccf9dc
Antonio Vivone <ndonio96@gmail.com>
2019-08-08 19:41:35 +0200

    added env settings

--- IDWork/app.js
+++ IDWork/app.js
@@ -26,7 +26,8 @@
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
 
-
+//setting the use of .env file
+require('dotenv').config();
 
 // Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
 var sails;
