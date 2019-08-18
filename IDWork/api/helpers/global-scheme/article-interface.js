c2f6e64080100d26a1256264a59ba4a58124b495
Antonio Vivone <ndonio96@gmail.com>
2019-08-18 11:57:57 +0200

    added call to medium and hackernoon

--- IDWork/api/helpers/global-scheme/article-interface.js
+++ IDWork/api/helpers/global-scheme/article-interface.js
@@ -4,27 +4,41 @@ module.exports = {
   friendlyName: 'Article interface',
 
 
-  description: '',
+  description: 'Helper that retrieves all articles on Medium and Hackernoon related to the given keyword (Tag)',
 
 
   inputs: {
-
+    keyword: {
+      description: 'The keyword (Tag) given as input',
+      type: 'string',
+      required: true
+    }
   },
 
 
   exits: {
 
     success: {
-      description: 'All done.',
+      description: 'Output all articles from Medium and Hackernoon.',
     },
 
   },
 
 
-  fn: async function (inputs) {
-    // Antonio copia la roba che mi avevi fatto vedere
+  fn: async function ({keyword}) {
+    wrappers_folder = sails.config.custom.wrappers_folder;
+
+    keyword = keyword.replace(/[^A-Za-z0-9]+/g, '');
+
+    const medium = require(`${wrappers_folder}medium_wrapper`);
+    let medium_result = await medium.getPosts(keyword);
+
+    const hackernoon = require(`${wrappers_folder}hackernoon_wrapper`);
+    let hackernoon_result = await hackernoon.getPosts(keyword);
+
+    let articles = medium_result.concat(hackernoon_result);
+
+    return articles;
   }
-
-
 };
 
